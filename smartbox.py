7da6279779b8dc636120e237893cc21cd81c7ce0
Dragos <blaj.dragos@gmail.com>
2013-01-28 00:38:58 +0200

    Changes to brainiac

--- smartbox.py
+++ smartbox.py
@@ -6,23 +6,50 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
-def enum(*sequential, **named):
-    enums = dict(zip(sequential,range(len(sequential))),**named)
-    return type('Enum',(),enums)
+WordDelim = ['Space','Tab']
+LineDelim = ['NewLine']
 
-BrainiacStates = enum('Linear','LR')
+BrainiacStates = ['Linear','LR']
+BrainiacCursorEvents = ['MoveLeft','MoveRight','MoveUp','MoveDown']
+BrainiacKeyEvents = ['TypeWordDelim','TypeLineDelim','TypeAbc']
 
 class Brainiac:
-    state = BrainiacStates.Linear;
-
+    state = BrainiacStates['Linear'];
+    def checkState():
+        pass
+    def changeState():
+        pass
+    def LinearMethod():
+        pass
+    def LRMethod():
+        pass
     def processEvent():
         pass
+    def mapKeyCodeToEvent(keycode):
+        # keycodes are used from PyQt4
+        if keycode == 0x01000012:
+            return BrainiacCursorEvents.['MoveLeft']
+        elif keycode == 0x01000014:
+            return BrainiacCursorEvents['MoveRight']
+        elif keycode == 0x01000013:
+            return BrainiacCursorEvents['MoveUp']
+        elif keycode == 0x01000015:
+            return BrainiacCursorEvents['MoveDown']
+        elif keycode == 0x01000004:
+            return BrainiacKeyEvents['TypeLineDelim']
+        elif keycode == 0x01000001:
+            return == BrainiacKeyEvents['TypeWordDelim']
+        elif keycode == 0x20:
+            return == BrainiacKeyEvents['TypeWordDelim']
+        elif keycode >= 0x21 and keycode <= 0x0ff:
+            return == keycode
 
 class SmartTextbox(QtGui.QTextEdit):
     def __init__(self):
         super(SmartTextbox, self).__init__()
     
     def keyPressEvent(self , event):
+        print(str(event.key())+" : "+event.text())
         QtGui.QTextEdit.keyPressEvent(self, event)
 
 class Window(QtGui.QMainWindow):
